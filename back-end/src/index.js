ddc86c7948e6d0df3289dbeb1bd19e1bcae3811f
Alexandre Ramos dos Santos <alexandresantos84@gmail.com>
2018-11-07 13:36:03 -0200

    modificaçao da nome da pastas de server para back-end

---
+++ back-end/src/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const app = express()
+const port = process.env.PORT || 3001
+
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+require('dotenv').load()
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true
+})
+
+app.use((req, res, next) => {
+  req.io = io
+
+  return next()
+})
+
+app.use(cors())
+app.use(express.json())
+app.use(require('./routes'))
+
+server.listen(port, () => {
+  console.log(`Server started on port http://localhost:${port}`)
+})
